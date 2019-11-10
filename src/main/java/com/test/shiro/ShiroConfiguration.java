c1e056302afe5243b9842b228d8c295ae3cb1f5a
junqiang5014 <502337752@qq.com>
2019-11-10 17:58:41 +0800

    初步的shiro

---
+++ src/main/java/com/test/shiro/ShiroConfiguration.java
@@ -0,0 +1,56 @@
+package com.test.shiro;
+
+import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfiguration {
+
+    @Bean(name = "shiroFilter")
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(SecurityManager securityManager){
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        shiroFilterFactoryBean.setLoginUrl("/loginPage");
+        shiroFilterFactoryBean.setSuccessUrl("/indexPage");
+        shiroFilterFactoryBean.setUnauthorizedUrl("/unauthPage");
+        Map<String, String> map = new HashMap<>();
+        map.put("loginPage", "anon");
+        map.put("login", "anon");
+        map.put("/**", "authc");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(map);
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean(name = "securityManager")
+    public SecurityManager getSecurityManager(MyRealm myRealm){
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager(myRealm);
+        return securityManager;
+    }
+
+    @Bean(name = "myRealm")
+    public MyRealm getMyRealm(HashedCredentialsMatcher hashedCredentialsMatcher){
+        MyRealm realm = new MyRealm();
+        realm.setCredentialsMatcher(hashedCredentialsMatcher);
+        return realm;
+    }
+
+    @Bean(name = "hashedCredentialsMatcher")
+    public HashedCredentialsMatcher getCredentialsMatcher(){
+        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher("md5");
+        hashedCredentialsMatcher.setHashIterations(5);
+        return hashedCredentialsMatcher;
+    }
+
+    @Bean(name = "shiroDialect")
+    public ShiroDialect getShiroDialect(){
+        return new ShiroDialect();
+    }
+}
