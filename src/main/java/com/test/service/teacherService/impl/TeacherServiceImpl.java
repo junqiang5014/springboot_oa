90a3529bda3877a538009545d703f15ec6a71093
junqiang5014 <502337752@qq.com>
2019-11-11 16:43:51 +0800

    aa

--- src/main/java/com/test/service/teacherService/impl/TeacherServiceImpl.java
+++ src/main/java/com/test/service/teacherService/impl/TeacherServiceImpl.java
@@ -1,4 +1,39 @@
 package com.test.service.teacherService.impl;
 
-public class TeacherServiceImpl {
+import com.test.mapper.teacherMapper.TeacherMapper;
+import com.test.pojo.Teacher;
+import com.test.service.teacherService.TeacherService;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.List;
+
+public class TeacherServiceImpl implements TeacherService {
+
+    @Qualifier
+    public TeacherMapper teacherMapper;
+
+    @Override
+    public int addTeacher(Teacher teacher) {
+        return teacherMapper.addTeacher(teacher);
+    }
+
+    @Override
+    public int deleteTeacher(int tid) {
+        return teacherMapper.deleteTeacher(tid);
+    }
+
+    @Override
+    public int updateTeacher(Teacher teacher) {
+        return teacherMapper.updateTeacher(teacher);
+    }
+
+    @Override
+    public Teacher getTeacherByTid(int tid) {
+        return teacherMapper.getTeacherByTid(tid);
+    }
+
+    @Override
+    public List<Teacher> getTeacherList() {
+        return teacherMapper.getTeacherList();
+    }
 }
