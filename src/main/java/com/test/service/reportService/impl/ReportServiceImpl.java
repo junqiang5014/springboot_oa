9499b0c5c4ced4f5ac1ad9e736f3b8d29c670c97
junqiang5014 <502337752@qq.com>
2019-11-11 21:03:07 +0800

    aa

--- src/main/java/com/test/service/reportService/impl/ReportServiceImpl.java
+++ src/main/java/com/test/service/reportService/impl/ReportServiceImpl.java
@@ -3,6 +3,7 @@ package com.test.service.reportService.impl;
 import com.test.mapper.reportMapper.ReportMapper;
 import com.test.pojo.Report;
 import com.test.service.reportService.ReportService;
+import org.activiti.engine.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,8 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private ReportMapper reportMapper;
+    @Autowired
+    private TaskService taskService;
 
     @Override
     public int addReport(Report report) {
@@ -26,6 +29,9 @@ public class ReportServiceImpl implements ReportService {
 
     @Override
     public int updateReport(Report report) {
+        String rid = String.valueOf(report.getRid());
+        String id = taskService.createTaskQuery().processInstanceBusinessKey(rid).singleResult().getId();
+        taskService.complete(id);
         return reportMapper.updateReport(report);
     }
 
