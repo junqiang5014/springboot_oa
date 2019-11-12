843a3b2f674c26a066671fe869a65ebb67c522ec
lzw1208 <2428144665@qq.com>
2019-11-12 14:53:55 +0800

    修改

--- src/main/java/com/test/controller/usersController/UsersController.java
+++ src/main/java/com/test/controller/usersController/UsersController.java
@@ -5,10 +5,10 @@ import com.test.service.userService.UsersService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.mgt.SubjectDAO;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
@@ -62,6 +62,24 @@ public class UsersController {
         }
 
         return "loginPage";
+    }
+
+    @RequestMapping("editUpwd")
+    public String editUpwd(HttpSession session, Model model){
+        User user = (User) session.getAttribute("user1");
+        String upwd = usersService.getUpwdByUname(user.getUname());
+        model.addAttribute(upwd);
+        return "updateUpwd";
+    }
+
+    @RequestMapping("updateUpwd")
+    public String updateUpwd(User user){
+        int i = usersService.updateUpwdByUser(user);
+        if(i>0){
+            return "redirect:index";
+        }
+        return "redirect:editUpwd?uid="+user.getUid();
+
     }
 
 }
