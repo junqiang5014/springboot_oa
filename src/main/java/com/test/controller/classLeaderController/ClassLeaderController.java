9d5456b31278fcebb7f998c5e0b3e2a50399fb14
Demon-life <mathew_xin@163.com>
2019-11-12 11:12:05 +0800

    xin

---
+++ src/main/java/com/test/controller/classLeaderController/ClassLeaderController.java
@@ -0,0 +1,48 @@
+package com.test.controller.classLeaderController;
+
+
+import com.test.pojo.ClassLeader;
+import com.test.pojo.Report;
+import com.test.pojo.User;
+import com.test.service.classLeaderService.ClassLeaderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class ClassLeaderController {
+    @Autowired
+    private ClassLeaderService classLeaderService;
+
+    @RequestMapping("selectMessage")
+    public String selectMessage(Model model){
+         ClassLeader classLeader= classLeaderService.selectMessage(1);
+        model.addAttribute("classLeader",classLeader);
+        return "viewstu";
+    }
+
+
+    @RequestMapping("selectReport")
+    @ResponseBody
+    public String selectReport( ){
+        List<Report> reportList = classLeaderService.selectReport();
+        System.out.println(reportList);
+       // model.addAttribute("reportList",reportList);
+        return "success";
+    }
+
+    @RequestMapping("selectReportByStuname")
+    @ResponseBody
+    public String selectReportByStuname( ){
+        List<Report> reportList = classLeaderService.selectReportByStuname("xin");
+        System.out.println(reportList);
+        // model.addAttribute("reportList",reportList);
+        return "success";
+    }
+
+
+}
