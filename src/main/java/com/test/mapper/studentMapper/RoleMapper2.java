97e49fa96af6e7f9ff5ec0fbf0942d94aa6905a0
lzw1208 <2428144665@qq.com>
2019-11-12 14:03:24 +0800

    修改

---
+++ src/main/java/com/test/mapper/studentMapper/RoleMapper2.java
@@ -0,0 +1,8 @@
+package com.test.mapper.studentMapper;
+
+import com.test.pojo.Role;
+
+public interface RoleMapper2 {
+
+    public Role getRole();
+}
---
+++ src/main/java/com/test/mapper/studentMapper/UserMapper.java
@@ -0,0 +1,37 @@
+package com.test.mapper.studentMapper;
+
+import com.test.pojo.User;
+
+import java.util.List;
+
+public interface UserMapper {
+
+    /**
+     * 根据角色修改密码
+     * @param user
+     * @return
+     */
+    public int updateUpwdByUser(User user);
+
+    /**
+     * 根据姓名查询密码
+     * @param uname
+     * @return
+     */
+    public String getUpwdByUname(String uname);
+
+    /**
+     * 根据姓名查询角色
+     * @param uname
+     * @return
+     */
+    public String getRoleByUname(String uname);
+
+    /**
+     * 查询user
+     * @return
+     */
+    public List<User> getUserAndRole();
+
+
+}
