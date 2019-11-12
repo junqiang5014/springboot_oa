75ac5daf1487a519f4cde1e9cb914ac08f2ebebe
lzw1208 <2428144665@qq.com>
2019-11-12 14:50:24 +0800

    修改

--- src/main/java/com/test/service/userService/UsersService.java
+++ src/main/java/com/test/service/userService/UsersService.java
@@ -2,9 +2,38 @@ package com.test.service.userService;
 
 import com.test.pojo.User;
 
+import java.util.List;
+
 public interface UsersService {
 
     public User getUserByUname(String uname);
+
+    /**
+     * 根据姓名查询密码
+     * @param uname
+     * @return
+     */
+    public String getUpwdByUname(String uname);
+
+    /**
+     * 根据姓名查询角色
+     * @param uname
+     * @return
+     */
+    public String getRoleByUname(String uname);
+
+    /**
+     * 查询user
+     * @return
+     */
+    public List<User> getUserAndRole();
+
+    /**
+     * 根据角色修改密码
+     * @param user
+     * @return
+     */
+    public int updateUpwdByUser(User user);
 
 
 }
