14ecb13425fb69cfd36d0bdf15cd8c95d0f9c0c9
lzw1208 <2428144665@qq.com>
2019-11-13 11:52:13 +0800

    修改

--- src/main/java/com/test/mapper/studentMapper/StudentMapper.java
+++ src/main/java/com/test/mapper/studentMapper/StudentMapper.java
@@ -24,7 +24,19 @@ public interface StudentMapper {
 
     public int updateStudent(Student student);
 
+    /**
+     * 根据stuid查找学生
+     * @param stuid
+     * @return
+     */
     public Student getStudentByStuid(int stuid);
+
+    /**
+     * 根据uid查询学生对象
+     * @param uid
+     * @return
+     */
+    public Student getStudentByUid(int uid);
 
     //根据cid找到学生列表
     public List<Student> getStudentListByCid(int cid);
