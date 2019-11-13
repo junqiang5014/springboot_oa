73862da39e224ab41a6fd934ce15dc612bb31c0e
fulin <m15066283823@163.com>
2019-11-13 10:30:34 +0800

    aa

--- src/main/java/com/test/mapper/userMapper/UsersMapper.java
+++ src/main/java/com/test/mapper/userMapper/UsersMapper.java
@@ -38,4 +38,20 @@ public interface UsersMapper {
     public List<User> getUserAndRole();
 
 
+    /**
+     * 根据uid删除用户
+     */
+    public int deleteUser(int uid);
+
+    /**
+     * 根据用户名进行模糊搜索
+     */
+    public List<User> getUserByLikeUname(String uname);
+
+    /**
+     * 修改密码
+     */
+    public int updatePassword(int uid);
+
+
 }
