971a112433bd92c24ae4b645cec945257ffaf124
lzw1208 <2428144665@qq.com>
2019-11-13 14:49:31 +0800

    修改

---
+++ src/main/java/com/test/service/student_holidayService/Student_holidayService.java
@@ -0,0 +1,12 @@
+package com.test.service.student_holidayService;
+
+public interface Student_holidayService {
+
+    /**
+     * 学生发起请假
+     * @param stuid
+     * @param hid
+     * @return
+     */
+    public int addHoliday(int stuid,int hid);
+}
