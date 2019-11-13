9d6d20a96c04ed78776dc0523cfdb835c1a290aa
lzw1208 <2428144665@qq.com>
2019-11-13 19:11:24 +0800

    11

--- src/main/java/com/test/controller/studentController/StudentController.java
+++ src/main/java/com/test/controller/studentController/StudentController.java
@@ -1,8 +1,10 @@
 package com.test.controller.studentController;
 
+import com.test.pojo.Holiday;
 import com.test.pojo.Report;
 import com.test.pojo.Student;
 import com.test.pojo.User;
+import com.test.service.holidayService.HolidayService;
 import com.test.service.reportService.ReportService;
 import com.test.service.studentService.StudentService;
 import com.test.service.userService.UsersService;
@@ -23,6 +25,8 @@ public class StudentController {
     private ReportService reportService;
     @Autowired
     private UsersService usersService;
+    @Autowired
+    private HolidayService holidayService;
 
     public ReportService getReportService() {
         return reportService;
@@ -40,6 +44,11 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    /**
+     * 学生列表
+     * @param model
+     * @return
+     */
     @RequestMapping("getStudent")
     public String getStudent(Model model){
         List<Student> studentAndClassList = studentService.getStudentAndClassList();
@@ -47,6 +56,12 @@ public class StudentController {
         return "stuList";
     }
 
+    /**
+     *跳转到修改页面
+     * @param model
+     * @param stuid
+     * @return
+     */
     @RequestMapping("editstudent")
     public String editstudent(Model model,int stuid){
         Student student = studentService.getStudentByStuid(stuid);
@@ -54,6 +69,12 @@ public class StudentController {
         return "updatemessage";
     }
 
+    /**
+     * 修改学生信息
+     * @param student
+     * @param session
+     * @return
+     */
     @RequestMapping("updatestudent")
     public String updatestudent(Student student, HttpSession session){
         User user=(User) session.getAttribute("user1");
@@ -65,6 +86,12 @@ public class StudentController {
 
     }
 
+    /**
+     * 查询学生信息
+     * @param session
+     * @param model
+     * @return
+     */
     @RequestMapping("getStudentmessage")
     public String getStudentByUser(HttpSession session,Model model){
         User user = (User)session.getAttribute("user1");
@@ -74,21 +101,30 @@ public class StudentController {
 
     }
 
-
-
-
-
+    /**
+     * 跳转到增加页面
+     * @return
+     */
     @RequestMapping("saveReport")
     public String saveReport(){
         return "add";
 
     }
 
+    /**
+     * 跳转到周报管理页面
+     * @return
+     */
     @RequestMapping("getreportmessage")
     public String getreportmessage(){
         return "reportmessage";
     }
 
+    /**
+     * 增加周报
+     * @param report
+     * @return
+     */
     @RequestMapping("addReport")
         public String addReport(Report report){
         int i = reportService.addReport(report);
@@ -98,6 +134,11 @@ public class StudentController {
         return "redirect:saveReport";
     }
 
+    /**
+     * 删除周报
+     * @param rid
+     * @return
+     */
     @RequestMapping("deleteReport")
     @ResponseBody
     public String deleteReport(int rid){
@@ -110,6 +151,11 @@ public class StudentController {
 
     }
 
+    /**
+     * 周报列表
+     * @param model
+     * @return
+     */
     @RequestMapping("getReport")
     public String getReport(Model model){
         List<Report> reportList = reportService.getReportList();
@@ -118,7 +164,11 @@ public class StudentController {
     }
 
 
-
+    /**
+     * 跳转到修改密码页面
+     * @param session
+     * @return
+     */
     @RequestMapping("editUpwd")
     public String updateUpwd(HttpSession session){
         User user = (User) session.getAttribute("user1");
@@ -126,6 +176,13 @@ public class StudentController {
         return "redirect:updateUpwd";
     }
 
+    /**
+     * 修改密码
+     * @param upwd
+     * @param newPwd
+     * @param cfgPwd
+     * @return
+     */
     @RequestMapping("updateUpwd")
     public String editUpwd(String upwd, String newPwd, String cfgPwd){
         int i = usersService.updateUserPwd(upwd,newPwd,cfgPwd);
@@ -135,6 +192,24 @@ public class StudentController {
         return "redirect:editUpwd";
 
     }
+
+    /**
+     * 学生请假
+     * @return
+     */
+    @RequestMapping("addHoliday")
+    public String addHoliday(Holiday holiday,HttpSession session,Model model){
+        User user=(User)session.getAttribute("user");
+        Student student = studentService.getStudentByUid(user.getUid());
+        model.addAttribute("student",student);
+        int i = holidayService.addHoliday(holiday);
+        if(i>0){
+            return "";
+        }
+        return "";
+    }
+
+
 
 
 
