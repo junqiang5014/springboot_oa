b4815d8336b2a729288bfbf9768937113794920b
fulin <m15066283823@163.com>
2019-11-14 11:22:55 +0800

    aa

--- src/main/java/com/test/service/userService/UsersServiceImpl.java
+++ src/main/java/com/test/service/userService/UsersServiceImpl.java
@@ -2,6 +2,12 @@ package com.test.service.userService;
 
 import com.test.mapper.userMapper.UsersMapper;
 import com.test.pojo.User;
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.crypto.hash.Md5Hash;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -90,5 +96,49 @@ public class UsersServiceImpl implements UsersService {
     public int updatePassword(User user) {
         return usersMapper.updatePassword(user);
     }
+
+    @Override
+    public int addUser(User user) {
+        String uname = getPingYin(user.getUname());
+        String upwd = new Md5Hash(user.getUpwd()).toString();
+        user.setUname(uname);
+        user.setUpwd(upwd);
+        int i = usersMapper.addUser(user);
+        if(i>0){
+            return user.getUid();
+        }else {
+            return -1;
+        }
+
+    }
+
+
+    public static String getPingYin(String src) {
+        char[] t1 = null;
+        t1 = src.toCharArray();
+        String[] t2 = new String[t1.length];
+        HanyuPinyinOutputFormat t3 = new HanyuPinyinOutputFormat();
+        t3.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        t3.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        t3.setVCharType(HanyuPinyinVCharType.WITH_V);
+        String t4 = "";
+        int t0 = t1.length;
+        try {
+            for (int i = 0; i < t0; i++) {
+                // 判断是否为汉字字符
+                if (java.lang.Character.toString(t1[i]).matches("[\\u4E00-\\u9FA5]+")) {
+                    t2 = PinyinHelper.toHanyuPinyinStringArray(t1[i], t3);
+                    t4 += t2[0];
+                } else {
+                    t4 += java.lang.Character.toString(t1[i]);
+                }
+            }
+            return t4;
+        } catch (BadHanyuPinyinOutputFormatCombination e1) {
+            e1.printStackTrace();
+        }
+        return t4.toLowerCase();
+    }
+
 
 }
