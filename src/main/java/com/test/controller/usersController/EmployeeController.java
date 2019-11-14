2c93d340706c1070cfefa6a1862f14f93f2f7164
fulin <m15066283823@163.com>
2019-11-14 11:20:38 +0800

    aa

---
+++ src/main/java/com/test/controller/usersController/EmployeeController.java
@@ -0,0 +1,292 @@
+package com.test.controller.usersController;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.test.pojo.*;
+import com.test.service.userService.EmployeeService;
+import com.test.service.userService.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private UsersService usersService;
+
+
+    @RequestMapping("info")
+    public String getinfo(){
+        return "employeetable";
+    }
+
+
+    @RequestMapping("getAllEmployee")
+    public String getAllEmployee(@RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "1")int pageNum1, @RequestParam(defaultValue = "1")int pageNum2, Model model, HttpSession session){
+
+        if(pageNum!=1){
+            session.setAttribute("pageNum",pageNum);
+        }
+        if(pageNum1!=1){
+            session.setAttribute("pageNum1",pageNum1);
+        }
+        if(pageNum2!=1){
+            session.setAttribute("pageNum2",pageNum2);
+        }
+
+        if(pageNum==1){
+            if(session.getAttribute("pageNum")==null){
+                pageNum=pageNum;
+            }else {
+                pageNum= (Integer) session.getAttribute("pageNum");
+            }
+
+        }
+        if (pageNum1==1){
+            if(session.getAttribute("pageNum1")==null){
+                pageNum1=pageNum1;
+            }else {
+                pageNum1= (Integer) session.getAttribute("pageNum1");
+            }
+        }
+        if(pageNum2==1){
+            if(session.getAttribute("pageNum2")==null){
+                pageNum2=pageNum2;
+            }else {
+                pageNum2= (Integer) session.getAttribute("pageNum2");
+            }
+        }
+
+
+
+        int[]page = new int[3];
+        page[0]=pageNum;
+        page[1]=pageNum1;
+        page[2]=pageNum2;
+        Map<String, Object> employee = employeeService.getEmployee(page);
+
+        PageHelper.startPage(pageNum,5);
+        List<Teacher> teacherList = (List<Teacher>) employee.get("teacherList");
+        PageInfo<Teacher> pageInfo = new PageInfo<Teacher>(teacherList);
+
+        PageHelper.startPage(pageNum1,4);
+        List<ClassLeader> classLeaderList = (List<ClassLeader>) employee.get("classLeaderList");
+        PageInfo<ClassLeader> pageInfo1 = new PageInfo<ClassLeader>(classLeaderList);
+
+        PageHelper.startPage(pageNum2,4);
+        List<SchoolManager> schoolManagerList = (List<SchoolManager>) employee.get("schoolManagerList");
+        PageInfo<SchoolManager> pageInfo2 = new PageInfo<SchoolManager>(schoolManagerList);
+
+        Map<String,Object> map = new HashMap<String, Object>();
+        map.put("pageInfo",pageInfo);
+        map.put("pageInfo1",pageInfo1);
+        map.put("pageInfo2",pageInfo2);
+//        model.addAttribute("pageInfo",pageInfo);
+        model.addAllAttributes(map);
+
+
+        return "employeetable";
+    }
+
+    @RequestMapping("changeTeacher")
+    public String changeTeacher(Teacher teacher){
+        int i = employeeService.changeEmployee(teacher);
+        return "redirect:/getAllEmployee";
+    }
+
+    @RequestMapping("changeClassLeader")
+    public String changeClassLeader(ClassLeader classLeader){
+        int i  = employeeService.changeEmployee(classLeader);
+        return "redirect:/getAllEmployee";
+    }
+
+    @RequestMapping("changeSchoolManager")
+    public String changeSchoolManager(SchoolManager schoolManager){
+        int i = employeeService.changeEmployee(schoolManager);
+        return "redirect:/getAllEmployee";
+    }
+
+    @RequestMapping("deleteTeacher")
+    public String deleteTeacher(int tid){
+        Teacher teacher = new Teacher();
+        teacher.setTid(tid);
+        employeeService.deleteEmployee(teacher);
+        return "redirect:/getAllEmployee";
+    }
+
+    @RequestMapping("deleteClassLeader")
+    public String deleteClassLeader(int clid){
+        ClassLeader classLeader = new ClassLeader();
+        classLeader.setClid(clid);
+        employeeService.deleteEmployee(classLeader);
+        return "redirect:/getAllEmployee";
+    }
+
+    @RequestMapping("deleteSchoolManager")
+    public String deleteSchoolManager(int smid){
+        SchoolManager schoolManager = new SchoolManager();
+        schoolManager.setSmid(smid);
+        employeeService.deleteEmployee(schoolManager);
+        return "redirect:/getAllEmployee";
+    }
+
+    @RequestMapping("addEmployee")
+    public String addEmployee(String name,String role){
+        if(role.equals("teacher")){
+
+            /**
+             * 添加user账户
+             */
+            User user = new User();
+            user.setUname(name);
+            user.setUpwd("123456");
+            Role role1 = new Role();
+            role1.setRoleid(2);
+            role1.setRolename("teacher");
+            user.setRole(role1);
+            int uid = usersService.addUser(user);
+            User user1 = new User();
+            user1.setUid(uid);
+            /**
+             * 添加教师信息,包含uid
+             */
+            Teacher teacher = new Teacher();
+            teacher.setTname(name);
+            teacher.setUser(user1);
+
+            int i = employeeService.addEmployee(teacher);
+            if(i>0){
+                return "redirect:/getAllEmployee";
+            }else
+                return "redirect:/getAllEmployee";
+
+        }else if (role.equals("classboss")){
+
+            User user = new User();
+            user.setUname(name);
+            user.setUpwd("123456");
+            Role role1 = new Role();
+            role1.setRoleid(5);
+            role1.setRolename("classboss");
+            user.setRole(role1);
+            int uid = usersService.addUser(user);
+            User user1 = new User();
+            user1.setUid(uid);
+
+            ClassLeader classLeader = new ClassLeader();
+            classLeader.setClname(name);
+            classLeader.setUser(user1);
+            employeeService.addEmployee(classLeader);
+            return "redirect:/getAllEmployee";
+        }else if (role.equals("schoolboss")){
+
+            User user = new User();
+            user.setUname(name);
+            user.setUpwd("123456");
+            Role role1 = new Role();
+            role1.setRoleid(4);
+            role1.setRolename("schoolboss");
+            user.setRole(role1);
+            int uid = usersService.addUser(user);
+            User user1 = new User();
+            user1.setUid(uid);
+
+            SchoolManager schoolManager = new SchoolManager();
+            schoolManager.setSmname(name);
+            schoolManager.setUser(user1);
+            employeeService.addEmployee(schoolManager);
+            return "redirect:/getAllEmployee";
+        }else{
+            System.out.println("类型匹配异常");
+            return "redirect:/getAllEmployee";
+        }
+
+
+    }
+
+    @RequestMapping("getAllEmployeeByLike")
+    public String getAllEmployeeByLike(String name,@RequestParam(defaultValue = "1") int pageNum3, @RequestParam(defaultValue = "1")int pageNum4, @RequestParam(defaultValue = "1")int pageNum5, Model model, HttpSession session){
+
+        if(name!=null){
+            session.setAttribute("name",name);
+        }
+
+        name= (String) session.getAttribute("name");
+
+
+        if(pageNum3!=1){
+            session.setAttribute("pageNum",pageNum3);
+        }
+        if(pageNum4!=1){
+            session.setAttribute("pageNum1",pageNum4);
+        }
+        if(pageNum5!=1){
+            session.setAttribute("pageNum2",pageNum5);
+        }
+
+        if(pageNum3==1){
+            if(session.getAttribute("pageNum3")==null){
+                pageNum3=pageNum3;
+            }else {
+                pageNum3= (Integer) session.getAttribute("pageNum3");
+            }
+
+        }
+        if (pageNum4==1){
+            if(session.getAttribute("pageNum4")==null){
+                pageNum4=pageNum4;
+            }else {
+                pageNum4= (Integer) session.getAttribute("pageNum4");
+            }
+        }
+        if(pageNum5==1){
+            if(session.getAttribute("pageNum5")==null){
+                pageNum5=pageNum5;
+            }else {
+                pageNum5= (Integer) session.getAttribute("pageNum5");
+            }
+        }
+        int[]page = new int[3];
+        page[0]=pageNum3;
+        page[1]=pageNum4;
+        page[2]=pageNum5;
+
+        Map<String, Object> employee = employeeService.getEmployeeByLike(name, page);
+
+
+        List<Teacher> teacherList = (List<Teacher>) employee.get("teacherList");
+        PageInfo<Teacher> pageInfo = new PageInfo<Teacher>(teacherList);
+
+
+        List<ClassLeader> classLeaderList = (List<ClassLeader>) employee.get("classLeaderList");
+        PageInfo<ClassLeader> pageInfo1 = new PageInfo<ClassLeader>(classLeaderList);
+
+
+        List<SchoolManager> schoolManagerList = (List<SchoolManager>) employee.get("schoolManagerList");
+        PageInfo<SchoolManager> pageInfo2 = new PageInfo<SchoolManager>(schoolManagerList);
+
+        System.out.println();
+        Map<String,Object> map = new HashMap<String, Object>();
+        map.put("pageInfo",pageInfo);
+        map.put("pageInfo1",pageInfo1);
+        map.put("pageInfo2",pageInfo2);
+//        model.addAttribute("pageInfo",pageInfo);
+        map.put("like","like");
+        model.addAllAttributes(map);
+
+
+        return "employeetable";
+
+    }
+}
