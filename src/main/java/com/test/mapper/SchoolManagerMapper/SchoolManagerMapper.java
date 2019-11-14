630bef1688013eb8647c51b8396c9b2fb3d941d1
lzw1208 <2428144665@qq.com>
2019-11-14 18:41:59 +0800

    11

--- src/main/java/com/test/mapper/SchoolManagerMapper/SchoolManagerMapper.java
+++ src/main/java/com/test/mapper/SchoolManagerMapper/SchoolManagerMapper.java
@@ -1,6 +1,10 @@
 package com.test.mapper.SchoolManagerMapper;
 
+import com.test.pojo.AllColumn;
 import com.test.pojo.SchoolManager;
+
+import java.io.InputStream;
+import java.util.List;
 
 public interface SchoolManagerMapper {
 
@@ -16,6 +20,14 @@ public interface SchoolManagerMapper {
      * @return
      */
     public int updateSchoolManager(SchoolManager schoolManager);
+
+    //查看班级平均成绩
+    public List<AllColumn> selectScoreAvg(String cname);
+
+    //查看学生成绩走势
+    public List<AllColumn> selectScoreByStuname(String stuname);
+
+    public List getBankListByExcel(InputStream in, String fileName) throws Exception;
 
 
 }
