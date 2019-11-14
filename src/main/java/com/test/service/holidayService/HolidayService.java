a54fdfc982e22ee2b924466e9d14bb0386176931
lzw1208 <2428144665@qq.com>
2019-11-14 19:02:12 +0800

    11

--- src/main/java/com/test/service/holidayService/HolidayService.java
+++ src/main/java/com/test/service/holidayService/HolidayService.java
@@ -1,6 +1,9 @@
 package com.test.service.holidayService;
 
+import com.test.pojo.ClassLeader;
 import com.test.pojo.Holiday;
+import com.test.pojo.Student;
+import com.test.pojo.Teacher;
 
 import java.util.List;
 
@@ -18,4 +21,10 @@ public interface HolidayService {
     public int changeStateByHid(int hid);
 
     public int addteacherHoliday(Holiday holiday, int id);
+
+    public Student getStudentByHid(int hid);
+
+    public Teacher getTeacherByHid(int hid);
+
+    public ClassLeader getClassLeaderByHid(int hid);
 }
