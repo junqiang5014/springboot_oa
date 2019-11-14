19a456fb54744b884b6617a14ef4d68b8b70fc3f
lzw1208 <2428144665@qq.com>
2019-11-14 10:16:07 +0800

    11

---
+++ src/main/java/com/test/mapper/SchoolManagerMapper/SchoolManagerMapper.java
@@ -0,0 +1,14 @@
+package com.test.mapper.SchoolManagerMapper;
+
+import com.test.pojo.SchoolManager;
+
+public interface SchoolManagerMapper {
+
+    /**
+     * 根据UId查询校长信息
+     * @return
+     */
+    public SchoolManager getSchoolManagerByUid();
+
+
+}
