a491fb2e7bb4d942da43a50bf6b74d8c7e8d29dd
fulin <m15066283823@163.com>
2019-11-14 11:21:55 +0800

    aa

---
+++ src/main/java/com/test/mapper/userMapper/EmployeeMapper.java
@@ -0,0 +1,53 @@
+package com.test.mapper.userMapper;
+
+import com.test.pojo.ClassLeader;
+import com.test.pojo.SchoolManager;
+import com.test.pojo.Teacher;
+
+import java.util.List;
+
+public interface EmployeeMapper {
+
+    /**
+     * 查找所有员工 即
+     * 查找所有的讲师,班主任以及校长
+     */
+
+    public List<Teacher> getTeacher();
+    public List<ClassLeader> getClassLeader();
+    public List<SchoolManager> getSchoolManager();
+
+
+    /**
+     * 删除员工信息
+     * @param tid
+     * @return
+     */
+    public int deleteTeacher(int tid);
+    public int deleteClassLeader(int clid);
+    public int deleteSchoolManager(int smid);
+
+    /**
+     * 模糊查询员工信息
+     * @return
+     */
+    public List<Teacher> getTeacherByLike(String tname);
+    public List<ClassLeader> getClassLeaderByLike(String clname);
+    public List<SchoolManager> getSchoolManagerByLike(String smname);
+
+
+    /**
+     * 修改员工信息
+     */
+    public int updateTeacher(Teacher teacher);
+    public int updateClassLeader(ClassLeader classLeader);
+    public int updateSchoolManager(SchoolManager schoolManager);
+
+
+    /**
+     * 添加员工信息
+     */
+    public int addTeacher(Teacher teacher);
+    public int addClassLeader(ClassLeader classLeader);
+    public int addSchoolManager(SchoolManager schoolManager);
+}
